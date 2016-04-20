300c31bdc2df5699c9cea54a812644ece7721c70
CandyOgre <no.one.coin@gmail.com>
2016-04-20 15:26:54 +0300

    Add helpers for invite_card

--- imports/ui/pages/cards/invite_card.js
+++ imports/ui/pages/cards/invite_card.js
@@ -1,2 +1,18 @@
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+
 import './invite_card.html';
 import './invite_card.css';
+
+Template.invite_card.helpers({
+	inviteFrom() {
+		return 'John';
+	},
+	inviteTo() {
+		return 'GDG Group';
+	},
+});
+
+Template.invite_card.events({
+	// TODO: catch clicks on Accept and Decline links
+});
