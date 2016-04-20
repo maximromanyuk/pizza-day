6a0591cbfe4956932d8bf4f2967a2c7d3e7c5d16
CandyOgre <no.one.coin@gmail.com>
2016-04-20 14:44:14 +0300

    Fixed bug with submitting form

--- imports/ui/pages/cards/group_new_card.js
+++ imports/ui/pages/cards/group_new_card.js
@@ -1,2 +1,14 @@
 import './group_new_card.html';
 import './group_new_card.css';
+
+Template.group_new_card.events({
+	'submit .create_group': function(e) {
+		e.preventDefault();
+
+		let text = e.target.group_name.value;
+		alert(`Form confirmed! Group name: ${text}`);
+		
+		e.target.group_name.value = '';
+		$('#group_name').prop('disabled', true);
+	},
+});
