0932310e6e72c7e965bfd93fc49b3fdd2118177c
CandyOgre <no.one.coin@gmail.com>
2016-04-20 15:06:03 +0300

    Add invitesCounter helper

--- imports/ui/components/nav.js
+++ imports/ui/components/nav.js
@@ -6,3 +6,11 @@ import './nav.html';
 Template.nav.rendered = function() {
   $(".button-collapse").sideNav();
 };
+
+Template.nav.helpers({
+	invitesCounter() {
+		// TODO: return true invites counter
+		// made for simplicity, at prototyping stage now
+		return 10; 
+	},
+});
