3641e300de6fe0bc15a7ce8a4d415dd5998ceaf3
CandyOgre <no.one.coin@gmail.com>
2016-04-20 09:59:34 +0300

    Set up basic routes

---
+++ imports/startup/client/routes.js
@@ -0,0 +1,52 @@
+
+FlowRouter.route('/', {
+  name: "home",
+  action: function(params, queryParams) {
+    BlazeLayout.render('masterLayout', {
+      footer: "footer",
+      main: "home",
+      nav: "nav",
+    });
+  }
+});
+
+FlowRouter.route('/groups', {
+  name: "groups",
+  triggersEnter: [AccountsTemplates.ensureSignedIn],
+  action: function(params, queryParams) {
+    BlazeLayout.render('masterLayout', {
+      footer: "footer",
+      main: "groups",
+      nav: "nav",
+    });
+  }
+});
+
+FlowRouter.route('/invites', {
+  name: "invites",
+  triggersEnter: [AccountsTemplates.ensureSignedIn],
+  action: function(params, queryParams) {
+    BlazeLayout.render('masterLayout', {
+      footer: "footer",
+      main: "invites",
+      nav: "nav",
+    });
+  }
+});
+
+FlowRouter.notFound = {
+  action: function() {
+    BlazeLayout.render('masterLayout', {
+      footer: "footer",
+      main: "pageNotFound",
+      nav: "nav",
+    });
+  }
+};
+
+AccountsTemplates.configureRoute('changePwd');
+AccountsTemplates.configureRoute('forgotPwd');
+AccountsTemplates.configureRoute('resetPwd');
+AccountsTemplates.configureRoute('signIn');
+AccountsTemplates.configureRoute('signUp');
+AccountsTemplates.configureRoute('verifyEmail');
