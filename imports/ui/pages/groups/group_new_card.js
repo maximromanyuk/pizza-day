04bdd0880ae352a0e3f04ffc32a78ddcb293629c
CandyOgre <no.one.coin@gmail.com>
2016-05-18 20:17:42 +0300

    New group creation form with input for logo url

--- imports/ui/pages/groups/group_new_card.js
+++ imports/ui/pages/groups/group_new_card.js
@@ -4,17 +4,25 @@ import { $ } from 'meteor/jquery';
 import './group_new_card.html';
 import './group_new_card.css';
 
+Template.groupNewCard.rendered=function() {
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+}
+
 Template.groupNewCard.events({
-	'submit .create_group': function(e) {
+	'submit .createGroup': function(e) {
 		e.preventDefault();
 
 		let groupName = e.target.group_name.value;
-		Materialize.toast(`Group '${groupName}' created!`, 4000)
+		//TODO: check if it is a real link
+		let logoUrl = e.target.logo_url.value;
+		Materialize.toast(`Group '${groupName}' created! ${logoUrl}`, 4000)
 		
 		e.target.group_name.value = '';
+		e.target.logo_url.value = '';
 		// disable input after creating
 		// $('#group_name').prop('disabled', true);
 
-		Meteor.call('groups.insert', groupName);
+		// Meteor.call('groups.insert', groupName);
 	},
 });
