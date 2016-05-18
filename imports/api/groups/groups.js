88485344cde5bd58fb775278f9c29ffdc33d6ade
CandyOgre <no.one.coin@gmail.com>
2016-05-18 20:44:59 +0300

    Adding logoUrl to database & bug fixes

--- imports/api/groups/groups.js
+++ imports/api/groups/groups.js
@@ -7,7 +7,7 @@ export const Groups = new Mongo.Collection("groups");
 Meteor.methods({
   //TODO: createEvent, updateEvent, finishEvent
   // methods for adding/updating items in order here too
-  'groups.insert'(groupName) {
+  'groups.insert'(groupName, logoUrl) {
   	check(groupName, String);
 
   	// Make sure the user is logged in before inserting a task
@@ -17,7 +17,7 @@ Meteor.methods({
 
   	Groups.insert({
   		name: groupName,
-      //TODO: insert logo
+      logoUrl: logoUrl,
       creator: Meteor.userId(),
       users: [Meteor.userId()]
   	});
--- imports/ui/pages/groups/group_new_card.js
+++ imports/ui/pages/groups/group_new_card.js
@@ -1,28 +1,32 @@
 import { Template } from 'meteor/templating';
+import { Materialize } from 'meteor/materialize:materialize';
 import { $ } from 'meteor/jquery';
 
 import './group_new_card.html';
 import './group_new_card.css';
 
-Template.groupNewCard.rendered=function() {
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-}
-
 Template.groupNewCard.events({
 	'submit .createGroup': function(e) {
 		e.preventDefault();
 
-		let groupName = e.target.group_name.value;
+		const groupName = e.target.group_name.value;
 		//TODO: check if it is a real link
-		let logoUrl = e.target.logo_url.value;
-		Materialize.toast(`Group '${groupName}' created! ${logoUrl}`, 4000)
+		const logoUrl = e.target.logo_url.value;
 		
 		e.target.group_name.value = '';
 		e.target.logo_url.value = '';
 		// disable input after creating
 		// $('#group_name').prop('disabled', true);
 
-		// Meteor.call('groups.insert', groupName);
+		Meteor.call('groups.insert', 
+			groupName,
+			logoUrl,
+			(err, res) => {
+			 	if(err) {
+			 		alert(err);
+			 	} else {
+					Materialize.toast(`Group '${groupName}' created!`, 4000);
+			 	}
+			 });
 	},
 });
