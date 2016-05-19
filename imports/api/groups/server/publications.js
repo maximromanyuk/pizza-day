93f7a4fd0e446004a4a23e238330742cf504ceb4
CandyOgre <no.one.coin@gmail.com>
2016-05-19 11:02:12 +0300

    Add check if owner for displaying Event management button

--- imports/api/groups/server/publications.js
+++ imports/api/groups/server/publications.js
@@ -5,3 +5,7 @@ import { Groups } from '../groups.js';
 Meteor.publish("groups", function() {
   return Groups.find();
 });
+
+Meteor.publish("group", function(id) {
+  return Groups.find(id);
+});
--- imports/ui/pages/group_page/group_page.js
+++ imports/ui/pages/group_page/group_page.js
@@ -1,4 +1,28 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Tracker } from 'meteor/tracker';
+
+import { Groups } from '../../../api/groups/groups.js';
+
 import './group_page.html';
 
 import './menu.js';
 import './users.js';
+
+Template.groupPage.onCreated(function groupPageOnCreated() {
+	this.autorun(() => {
+		const id = FlowRouter.getParam('groupId');
+		this.subscribe('group', id);
+	});
+	// Meteor.subscribe('group', id);
+});
+
+	Template.groupPage.helpers({
+		groupCreator() {
+			let id = FlowRouter.getParam('groupId');
+			if (!Groups.findOne(id)) return;
+			
+			return Meteor.userId() === Groups.findOne(id).creator;
+		}
+	});
