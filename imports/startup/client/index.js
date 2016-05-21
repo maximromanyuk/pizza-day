1be5915da4ff7ad3e04525c33f90aeeebc3226e2
CandyOgre <no.one.coin@gmail.com>
2016-05-21 16:35:14 +0300

    Fixed bug when notifications showed on every startup

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -3,3 +3,6 @@ import './at-config.js';
 
 // Set up routes
 import './routes.js';
+
+// used for listening for new invites
+import './startup.js';
--- imports/startup/client/startup.js
+++ imports/startup/client/startup.js
@@ -1,12 +1,18 @@
 import { Meteor } from 'meteor/meteor';
-import { Materialize } from 'materialize:materialize';
+import { Materialize } from 'meteor/materialize:materialize';
 
 import { Invites } from '../../api/invites/invites.js';
 
 Meteor.startup(() => {
-	Invites.find().observechanges({
-		added: function() {
-			Materialize.toast('New invite!', 4000);
-		}
-	});
+	(function() {
+		var initializing = true;
+  		Invites.find().observeChanges({
+    		added() {
+      			if (!initializing) {
+        			Materialize.toast('New invite!', 4000);
+      			}
+    		}
+  	});
+  	initializing = false;
+	})();
 });
