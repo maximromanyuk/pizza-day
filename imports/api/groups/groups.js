15e6a07c9b4e28537d34052edff78975cbe32c0a
CandyOgre <no.one.coin@gmail.com>
2016-05-25 16:33:07 +0300

    Remove all invites from group on group removing

--- imports/api/groups/groups.js
+++ imports/api/groups/groups.js
@@ -1,3 +1,19 @@
 import { Mongo } from 'meteor/mongo';
 
-export const Groups = new Mongo.Collection('groups');
+import { Invites } from '../invites/invites.js';
+
+class GroupsCollection extends Mongo.Collection {
+ remove(selector, callback) {
+  console.log(selector);
+  Invites.remove({groupId: selector._id}, (err) => {
+   if(err) {
+    console.log(err);
+   } else {
+    console.log('Invites from this group was removed');
+   }
+  });
+  return super.remove(selector, callback);
+ }
+}
+
+export const Groups = new GroupsCollection('groups');
--- imports/ui/pages/group_page/group_page.js
+++ imports/ui/pages/group_page/group_page.js
@@ -4,7 +4,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Materialize } from 'meteor/materialize:materialize';
 
 import { Groups } from '../../../api/groups/groups.js';
-// import { removeGroup } from '../../../api/groups/methods.js';
+import { removeGroup } from '../../../api/groups/methods.js';
 import { removeUserFromGroup } from '../../../api/groups/methods.js';
 
 import './group_page.html';
@@ -49,19 +49,23 @@ Template.groupPage.helpers({
 });
 
 Template.groupPage.events({
- // 'click #deleteGroup'() {
- //  const groupId = FlowRouter.getParam('groupId');
- //  removeGroup.call({
- //   groupId,
- //  }, (err) => {
- //   if(err) {
- //    console.log(err);
- //   } else {
- //    Materialize.toast('Group removed permanently', 4000);
- //   }
- //  });
- // },
- 'click #leaveGroupModal'() {
+ 'click #deleteGroup'() {
+  const groupId = FlowRouter.getParam('groupId');
+
+  removeGroup.call({
+   groupId,
+  }, (err) => {
+   if(err) {
+    console.log(err);
+   } else {
+    FlowRouter.go('groups');
+    Materialize.toast('Group removed permanently', 4000);
+   }
+  });
+ },
+ 'click #leaveOrRemoveGroupModal'() {
+  // $('#modal2').appendTo('body');
+  // $('#modal2').css('z-index', '1500');
   $('#modal2').openModal();
  },
  'click #leaveGroup'() {
