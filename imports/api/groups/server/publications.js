95575a317cf8ac1dcc13e6addffc83629297cff6
CandyOgre <no.one.coin@gmail.com>
2016-05-31 17:09:26 +0300

    Send less info about groups on groupsList page

--- imports/api/groups/server/publications.js
+++ imports/api/groups/server/publications.js
@@ -3,8 +3,16 @@ import { check } from 'meteor/check';
 
 import { Groups } from '../groups.js';
 
+Groups.publicFields = {
+ name: 1,
+ logoUrl: 1,
+ users: 1,
+};
+
 Meteor.publish('groups', () => {
- return Groups.find();
+ return Groups.find({}, {
+  fields: Groups.publicFields,
+ });
 });
 
 Meteor.publish('group', (id) => {
