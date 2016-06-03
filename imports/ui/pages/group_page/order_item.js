afd243d6d35c19111d1bb4583da7104aa61ff982
CandyOgre <no.one.coin@gmail.com>
2016-06-03 13:07:28 +0300

    Bug fix

--- imports/ui/pages/group_page/order_item.js
+++ imports/ui/pages/group_page/order_item.js
@@ -1,11 +1,15 @@
 import { Template } from 'meteor/templating';
+import { FlowRouter } from 'meteor/kadira:flow-router';
+
+import { Events } from '../../../api/events/events.js';
 
 import './order_item.html';
 
 Template.orderItem.helpers({
  canChangeQuantity() {
   const groupId = FlowRouter.getParam('groupId');
-  const event = Events.findOne(groupId);
+  const event = Events.findOne({ groupId: groupId });
+  if(!event) return;
 
   return event.status === 'ordering';
  },
